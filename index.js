c7dc9efee007fa9596cec112849bf653ce8702de
MaistroUwei <0208735@up.edu.mx>
2022-10-24 17:31:12 -0500

    Primer commit

---
+++ index.js
@@ -0,0 +1,18 @@
+// Importando librerías necesarias
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const { router } = require('./routes')
+
+//Creando app de express
+const app = express();
+const apiPort= process.env.Api_PORT || 3003;
+
+//Preparando/Seteando app de express
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(cors());
+app.use(bodyParser.json());
+app.use('/api', router);
+
+//Diciendole a la app que awante por nuevas llamadas y duerma cuando nadie llegue
+app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`));
